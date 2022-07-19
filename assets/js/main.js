8477ab95ffab027a26873e5f273aabe0a950c1c9
MD <dhmedy@googlemail.com>
2022-07-19 15:59:14 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+
+
+function fnChangeToGreen() {
+    document.body.style.backgroundColor = "green";
+}
+
+function fnChangeToPurple() {
+    document.body.style.backgroundColor = "purple";
+}
+
+function fnChangeToYellow() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function fnChnage434373() {
+    document.body.style.backgroundColor = "#434373";
+}
+
+function fnChnageToBlack() {
+    document.body.style.backgroundColor = "black";
+}
+
+function fnChnageToOrange() {
+    document.body.style.backgroundColor = "orange";
+}
+
+function fnChnageToDarkBlue() {
+    document.body.style.backgroundColor = "darkblue";
+}
+
+function fnChnageToLightBlue() {
+    document.body.style.backgroundColor = "lightblue";
+}
